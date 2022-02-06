08b2ec9be107e02ea1ca83af520e09aa5c03a029
Harsh Shah <hmshah9949@gmail.com>
2022-02-06 14:15:52 -0800

    Add form, listen to user input, using one state vs multiple states

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const onTitleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const onAmountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const onDateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={onTitleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            onChange={onAmountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            onChange={onDateChangeHandler}
+            min="2019-01-01"
+            max="2021-12-31"
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
